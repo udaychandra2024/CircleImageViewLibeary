deda56da569c805254b51e8d02f8942d98e43561
uday chandra <uday@example.com>
2024-08-31 15:21:47 +0600

    Initial commit by uday chandra

---
+++ app/src/main/java/com/ScircleImageuday/circleimagelibuc/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ScircleImageuday.circleimagelibuc;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    CircleimageLibeary uday;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
